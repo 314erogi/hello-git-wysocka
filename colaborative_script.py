b4ccdf336c0675b80d203b59a75f355c8c00757c
314erogi <goslaw55555@gmail.com>
2021-01-22 11:45:40 +0100

    print ascii code of letters

---
+++ colaborative_script.py
@@ -0,0 +1,8 @@
+s = "This is my first Python repository"
+l1 = []
+l2 = []
+for c in s:   # in Python, a string is just a sequence, so we can iterate over it!
+    l1.append(c) 
+    l2.append(ord(c))
+print(l1)
+print(l2)
